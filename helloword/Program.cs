30c5bb8be26fbe63d1bd24561d239e9d1629dde8
chelezo <alex.chelezo@gmail.com>
2021-12-15 12:56:16 +0200

    Добавьте файлы проекта.

---
+++ helloword/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace helloword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           String myUserName = Environment.UserName;
+
+
+            Console.WriteLine($"Добрый День { myUserName} сегодня [" + DateTime.Now + "]");
+            string name = Console.ReadLine();
+        }
+    }
+}
