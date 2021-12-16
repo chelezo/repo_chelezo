4c419b978ff88d8be256258e4d7d48dea3cf9d23
chelezo <alex.chelezo@gmail.com>
2021-12-16 16:01:47 +0200

    minmax_temp

---
+++ helloword/helloword/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace helloword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           String myUserName = Environment.UserName;
+
+            Console.WriteLine($"Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Добрый День { name} ваша учетная запись ПК '{ myUserName}' сегодня  [" + DateTime.Now + "]");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ minmaxtemp/minmaxtemp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace minmaxtemp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Введите минимальную температуру");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите максимальну температуру");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int summa = ((min + max) / 2);
+            Console.WriteLine($"Средняя температура { summa}");
+        }
+
+    }
+}
