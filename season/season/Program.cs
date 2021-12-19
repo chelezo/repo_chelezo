980bee3ad6be5d3b367baaa8a1153455e8ec88b2
chelezo <alex.chelezo@gmail.com>
2021-12-19 22:29:11 +0200

    измения enum

--- season/season/Program.cs
+++ season/season/Program.cs
@@ -5,58 +5,46 @@ namespace season
     class Program
     {
         [Flags]
-        public enum season 
-            {
-            
-        }
-        public  enum Spring
+       
+        public  enum Season
         {
-            March,
-            April,
-            May
+            Spring,
+            Summer,
+            Autum,
+            Winter
         }
-        public enum Summer
-        {
-            June,
-            July,
-            August
-        }
-        public enum Autumn
-        {
-            September,
-            October,
-            November
-        }
-        public enum Winter
-        {
-            December,
-            January,
-            February
-        }
+       
 
         static void Main(string[] args)
         {
-            
-            Spring  SeasonSpring = (Spring.April | Spring.March | Spring.May) ;
-           Spring AllSpringMonth = (Spring)0b000111;
-            
-            Console.WriteLine("Введите время года Spring, Summer, Autumn, Winter");
-             string EntSeason = Console.ReadLine();
+            //int intSpring = (int)Spring.April;
+            Season SeasonNaber = Season.Winter | Season.Summer | Season.Autum | Season.Spring;
+            Console.WriteLine("Введите время года Spring, Summer, Autumn, Winter"); 
+            string EntSeason = Console.ReadLine();
             string trfx = "Spring";
-             bool seasSpring = (EntSeason == trfx);
+            string trfx2 = "Winter";
+            bool seasSpring = (EntSeason == trfx);
+            bool seasWinter = (EntSeason == trfx2);
+
+            switch (EntSeason) 
+            {
+                case Winter:
+                    Console.WriteLine("Добрый день");
+
+            }
 
             //string seas = AllSpringMonth;
-           // Console.WriteLine($"Введите время года {SeasonSpring}");
-         if (seasSpring)
+            //Console.WriteLine($"Введите время года {SeasonSpring}");
+            if (seasSpring)
             {
-              Console.WriteLine($"Введите время года {AllSpringMonth}");
+              Console.WriteLine($"Месяцы {SeasonSpring}");
             }
-            else
-           {
-             Console.WriteLine("Введите время года lheujt");
+            else if (seasWinter)
+            {
+                Console.WriteLine($"Месяцы {seasWinter}");
             }
-            
-            
+
+            Console.ReadLine();
         }
     }
 }
